0f35224a92db71adc86eb1cdf7c36be2d5b39b76
Larysa Bilan <114293094+dudetu@users.noreply.github.com>
2022-10-04 14:39:51 +0300

    Add files via upload

---
+++ homwork1.java
@@ -0,0 +1,38 @@
+package Lessen1;
+
+public class homwork1 {
+    //09/27 boolean вычисления и long
+
+    public static void main(String[] args) {
+
+    long secund = 3011456_567l;
+    long minut = 60 ;
+    minut = secund / minut;
+    long chas = 60 ;
+    chas = minut / chas ;
+
+    long  day =  24;
+    day = chas / day ;
+
+    long  year = 360;
+    year = day / year;
+
+
+        System.out.println(  minut  +    "сколько минут прожито ");
+        System.out.println( chas  +   " ссколько часов прожито" );
+        System.out.println(  day   +   " сколько дней прожито ");
+                System.out.println( year  +   "сколько лет прожито ");
+
+    int time = 4 ;
+    boolean rigt = time > 5 ;
+
+    boolean  name = false ;
+    boolean  asterix = true ;
+    boolean   answer = rigt || name ;
+
+
+        System.out.println(answer );
+
+
+}
+}

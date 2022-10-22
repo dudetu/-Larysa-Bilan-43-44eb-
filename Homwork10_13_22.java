84a81b7e2218360b93b5c7e231cf8fc88ae6c5eb
Larysa Bilan <114293094+dudetu@users.noreply.github.com>
2022-10-22 21:06:59 +0300

    Add files via upload

---
+++ Homwork10_13_22.java
@@ -0,0 +1,33 @@
+package Homwork10_13_22;
+import java.util.Scanner;
+public class Homwork10_13_22 {
+    public static void main(String[] args) {
+
+        // First level: Вводится число. Напишите на экран сообщение
+        // "Я начинаю разбираться с циклами" количество раз, равное этому числу
+
+
+        // Second level: В программу вводится 1 число.
+        // ыведите на экран его факториал, используя цикл while
+        // https://math-prosto.ru/ru/pages/factorial/factorial/
+      //  Scanner scanner = new Scanner(System.in);
+
+
+     int  a =0;
+        while ( a <=4  ) {
+            a++;
+            System.out.println(  a + " Я начинаю разбираться с циклами");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
